ccfe8c5d2c7179151296a04592545acbc901fa83
giim <victor.oriedreh@gmail.com>
2018-01-18 18:44:19 +0000

    first working version

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # soundcloud-to-rss
+
+### Simple script outputting the RSS url for some [SoundCloud](https://soundcloud.com/) channels.
+
+
+#### Works by passing search keywords as arguements. For instance:
+
+```bash
+python3 s2rss.py studio404 paris
+```
---
+++ s2rss.py
@@ -0,0 +1,77 @@
+#! /usr/bin/python3
+
+from sys import argv, exit
+import re
+import urllib
+from bs4 import BeautifulSoup as bs
+from time import time
+
+
+BASE_URL = "https://soundcloud.com/"
+RSS_FORMAT = "http://feeds.soundcloud.com/users/soundcloud:users:{}/sounds.rss"
+
+class PageScrapper():
+	def __init__(self, delay_between_queries=0):
+		self.opener = urllib.request.build_opener()
+		self.opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+		self.lastQuery = -float("inf")
+		self.delay_between_queries = delay_between_queries
+	def readUrl( self, url_):
+		presentTime = time() - self.lastQuery - self.delay_between_queries
+		if presentTime < 0:
+			sleep( abs(presentTime))
+		inData = self.opener.open(url_)
+		content = inData.read()
+		self.lastQuery = time()
+		return bs(content, "html.parser")
+	def __call__( self, url_):
+		return self.readUrl( url_)
+
+def SearchUrl( search_items):
+	"""
+	Returns the soundcloud search url from search inputs
+	"""
+	return BASE_URL + "search/people?q=" + urllib.parse.quote_plus(search_items)
+
+class ResultItem:
+	def __init__(self, text, url):
+		self.text = text
+		self.url = url
+	def __repr__(self):
+		return "<search result for %s>" %self.text
+	
+
+if len(argv) > 1:
+	search_items = " ".join( argv[1:])
+else:
+	print("What soundcloud feed are you looking for?")
+	search_items = input()
+search_url = SearchUrl( search_items)
+scrapper = PageScrapper()
+
+
+
+search_soup = scrapper( search_url)
+try:
+	research_results = search_soup.find_all("ul")[1].find_all("a")
+	results = {index: ResultItem(x.text, x["href"][1:]) for index, x in enumerate(research_results,start=1)}
+except IndexError:
+	print( "Search gave no results..")
+	exit()
+else:
+	print()
+	for result_index, result in results.items():
+		print( "%2d:  %s" %(result_index, result.text))
+	user_index = None
+	print("\nWhich one is it? (enter index)")
+	while not user_index in results:
+		user_index = int( input())
+	sc_user_url = BASE_URL + results[user_index].url
+
+sc_user_soup = scrapper( sc_user_url)
+try:
+	sc_userid = next(re.finditer("users:(\d+)", str(sc_user_soup))).group(1)
+except StopIteration:
+	print( "Problem reading Soundcloud user ID")
+	exit()
+print( "\n\t%s" %(RSS_FORMAT.format(sc_userid)))
